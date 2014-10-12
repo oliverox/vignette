4e375b5955709327596ebc223420a48769058193
Oliver Oxenham <ooxenham@stubhub.com>
2014-10-12 15:27:43 -0700

    updated routes

--- src/components/other_component.js
+++ src/components/other_component.js
@@ -7,11 +7,11 @@ var React = require('react');
 var OtherComponent = React.createClass({
     render: function() {
         var thisStyle = {
-            color: 'red'
+            color: 'yellowgreen'
         };
         return (
             <div>
-                <h3 style={thisStyle}>Other Component</h3>
+                <h1 style={thisStyle}>Other Component</h1>
                 <a href="/">go to home</a>
             </div>
         );

c37bb38493f8910637c87c6eb834c120e084d210
Oliver Oxenham <ooxenham@stubhub.com>
2014-10-27 17:39:39 -0700

    prevent scrollY

--- src/components/other_component.js
+++ src/components/other_component.js
@@ -6,11 +6,25 @@ var React = require('react');
 // var Velocity = require("../../node_modules/velocity-animate/velocity");
 
 var OtherComponent = React.createClass({
+    componentDidMount: function() {
+        // var content = document.getElementById("scroller-content");
+        
+    },
     render: function() {
         // var thisStyle = {
         //     color: 'yellowgreen'
         // };
         // Velocity(document.body, { opacity: 0.5 });
+
+		var contentWidth = 1536;
+		var contentHeight = 1024;
+
+		// Initialize layout
+		// var container = document.getElementById("scroller-container");
+		var content = document.getElementById("scroller-content");
+
+		content.style.width = contentWidth + "px";
+		content.style.height = contentHeight + "px";
 
         return (
             <div id='scroller-container'>
